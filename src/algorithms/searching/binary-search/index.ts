55e055d7df5ab55e905c3789eb76e4f0c4dd60bf
HyungWoo Kim <erickim713@kaist.ac.kr>
2019-10-11 11:35:59 +0000

    [ADD] binary searching implementation

--- src/algorithms/searching/binary-search/index.ts
+++ src/algorithms/searching/binary-search/index.ts
@@ -1,6 +1,19 @@
 const binarySearch = (array: number[], target: number ) => {
   let lowIdx = 0, highIdx = array.length - 1;
-  /**
-    implement the loop here tomorrow
-   */
+  while(lowIdx <= highIdx) {
+    let midIdx = Math.floor((lowIdx + highIdx) / 2);
+    if(target === array[midIdx]) return midIdx;
+    else if (array[midIdx] > target) {
+      highIdx = midIdx - 1;
+    } else {
+      lowIdx = midIdx + 1;
+    }
+  }
+
+  return -1;
 }
+
+console.log(binarySearch([1, 2, 4, 6, 10, 11] , 4));
+console.log(binarySearch([1, 2, 4, 6, 10, 11] , 1));
+console.log(binarySearch([1, 2, 4, 6, 10, 11] , 7));
+console.log(binarySearch([1, 2, 4, 6, 10, 11] , 11));
